fa2b48fb72940ef85840467fb10ac8376d8e436c
Harini Gunisetty <gunisettyharini@gmail.com>
2021-02-18 06:37:34 +0530

    Added a last anme pattern

--- UserRegistration.java
+++ UserRegistration.java
@@ -9,16 +9,29 @@ public class UserRegistration {
 		Matcher match = pattern.matcher(name);
 		boolean bool = match.matches();// checks if the pattern matches with the given pattern and returns a boolean value 
 		if (bool) 
-			System.out.println(name + " is a valid name");
+			System.out.println(name + " is a valid first name");
 		else
-			System.out.println(name + " is not a valid name");
+			System.out.println(name + " is not a valid first name");
+	}
+	public static void lastNameCheck(String name) {
+		String regex = "^[A-Z]{1}[a-zA-Z]{2,}$"; // 1st letter is Capital and has minimum of 3 letters
+		Pattern pattern = Pattern.compile(regex);
+		Matcher match = pattern.matcher(name);
+		boolean bool = match.matches();// checks if the pattern matches with the given pattern and returns a boolean value 
+		if (bool) 
+			System.out.println(name + " is a valid last name");
+		else
+			System.out.println(name + " is not a valid last name");
 	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration Program");
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a valid first name");
 		String firstName = sc.next();
+		System.out.println("Enter a valid last name");
+		String lastName = sc.next();
 		firstNameCheck(firstName);
+		lastNameCheck(lastName);
 	}
 
 }
