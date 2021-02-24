882e8ce1f046c36d94f3d342bdee5359a1448915
Harini Gunisetty <gunisettyharini@gmail.com>
2021-02-24 19:19:04 +0530

    Refactored to add custom exception

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,36 +1,71 @@
 package com.userregistration;
 
+
 import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-    public boolean isFirstNamevalid(String firstName) {
-        return Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", firstName);
+    public static boolean isFirstNamevalid(String firstName) throws UserRegistrationException {
+
+         if(Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", firstName))
+             return true;
+         else
+             throw new UserRegistrationException("Invalid First Name");
     }
-    public boolean isLastNameValid(String lastName) {
-        return Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", lastName);
+    public static boolean isLastNameValid(String lastName) throws UserRegistrationException {
+        if(Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", lastName))
+            return true;
+        else
+            throw new UserRegistrationException("Invalid Last Name");
     }
-    public boolean isEmailValid(String email) {
-        return Pattern.matches("^[a-zA-Z0-9]{3,}([\\.\\+\\-]?[a-zA-Z0-9]{3,})?[@][A-Za-z0-9]{1,}[.][A-Za-z]{2,4}[,]?([.][A-Za-z]{2,4}[.]?)?$", email);
+    public static boolean isEmailValid(String email) throws UserRegistrationException {
+        if(Pattern.matches("^[a-zA-Z0-9]{3,}([\\.\\+\\-]?[a-zA-Z0-9]{3,})?[@][A-Za-z0-9]{1,}[.][A-Za-z]{2,4}[,]?([.][A-Za-z]{2,4}[.]?)?$", email))
+            return true;
+        else
+            throw new UserRegistrationException("Invalid Email");
+
     }
-    public boolean isMobileNumberValid(String mobile) {
-        return Pattern.matches("^[9][1][\\s][6-9][0-9]{9}$", mobile);
+    public static boolean isMobileNumberValid(String mobile) throws UserRegistrationException {
+        if(Pattern.matches("^[9][1][\\s][6-9][0-9]{9}$", mobile))
+            return true;
+        else
+            throw new UserRegistrationException("Invalid Mobile Number");
     }
-    public boolean isPasswordValid(String password) {
-        return Pattern.matches("^(?=.*[\\@\\#\\$\\%\\&\\_\\,\\.])(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}$", password);
+    public static boolean isPasswordValid(String password) throws UserRegistrationException {
+        if(Pattern.matches("^(?=.*[\\@\\#\\$\\%\\&\\_\\,\\.])(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}$", password))
+            return true;
+        else
+            throw new UserRegistrationException("Invalid Password");
     }
     public static void main(String[] args) {
-        String firstName = "Amit";
+        String firstName = "amit";
         String lastName = "Maharana";
         String email = "abc.xyz@gmail.com";
         String mobile = "91 8210029078";
         String password = "Ahjgsd45@fj";
         UserRegistration person = new UserRegistration();
-        person.isFirstNamevalid(firstName);
-        person.isLastNameValid(lastName);
-        person.isEmailValid(email);
-        person.isMobileNumberValid(mobile);
-        person.isPasswordValid(password);
+        try {
+            isFirstNamevalid(firstName);
+        }catch(Exception e) {System.out.println("Exception occured is " + e);}
+        try {
+            isLastNameValid(lastName);
+        }catch(Exception e) {
+            System.out.println("Exception occured is " + e);
+        }
+        try {
+            isEmailValid(email);
+        }catch(Exception e) {
+            System.out.println("Exception occured is " + e);
+        }
+        try {
+            isMobileNumberValid(mobile);
+        }catch(Exception e) {
+            System.out.println("Exception occured is " + e);
+        }
+        try {
+            isPasswordValid(password);
+        }catch(Exception e) {
+            System.out.println("Exception occured is " + e);
+        }
     }
 }
-
--- UserRegistrationTest.java
+++ UserRegistrationTest.java
@@ -3,88 +3,166 @@ package com.userregistration;
 import org.junit.Assert;
 import org.junit.Test;
 
-import javax.xml.xpath.XPathEvaluationResult;
-
 public class UserRegistrationTest {
     static UserRegistration person = new UserRegistration();
     @Test
     public void checkGivenFirstNameIsValidSouldRetuenTrue() { // Check if the pattern works for valid name
-        Assert.assertEquals(true, person.isFirstNamevalid("Amit"));
+        try {
+            Assert.assertEquals(true, person.isFirstNamevalid("Amit"));
+        }catch(Exception e) {
+            System.out.println("Exception occured is " + e);
+        }
     }
     @Test
     public void checkGivenFirstNameIsInvalidForlessThan3Characters() { // Checkif the pattern does not works for invalid name with less than 3 characters
-        Assert.assertEquals(false, person.isFirstNamevalid("Ab"));
+        try {
+            Assert.assertEquals(false, person.isFirstNamevalid("Ab"));
+        }catch(Exception e) {
+            System.out.println("Exception occured is " + e);
+        }
     }
     @Test
     public void checkGivenFirstNameisInvalidForFirstLetterLowerCase() { // Checks if there is no upper case in first Character
-        Assert.assertEquals(false, person.isFirstNamevalid("amit"));
+        try {
+            Assert.assertEquals(false, person.isFirstNamevalid("amit"));
+        }catch(Exception e) {
+            System.out.println("Exception occured is " + e);
+        }
     }
     @Test
     public void checkGivenFirstNameisInvalidForNumericVAlues() { //Checks if there is Numeric Digits in Name
-        Assert.assertEquals(false, person.isFirstNamevalid("Amit123"));
+        try {
+            Assert.assertEquals(false, person.isFirstNamevalid("Amit123"));
+        }catch(Exception e) {
+            System.out.println("Exception occured is " + e);
+        }
     }
     @Test
     public void checkGivenLastNameIsValid() { // Same as first name
-        Assert.assertEquals(true, person.isLastNameValid("Amit"));
+        try {
+            Assert.assertEquals(true, person.isLastNameValid("Amit"));
+        }catch(Exception e) {
+            System.out.println("Exception occured is " + e);
+        }
     }
     @Test
     public void checkGivenLastNameIsInvalidForlessThan3Characters() {
-        Assert.assertEquals(false, person.isLastNameValid("Ab"));
+        try {
+            Assert.assertEquals(false, person.isLastNameValid("Ab"));
+        }catch(Exception e) {
+            System.out.println("Exception occured is " + e);
+        }
     }
     @Test
     public void checkGivenLastNameisInvalidForFirstLetterLowerCase() {
-        Assert.assertEquals(false, person.isLastNameValid("amit"));
+        try {
+            Assert.assertEquals(false, person.isLastNameValid("amit"));
+        }catch(Exception e) {
+            System.out.println("Exception occured is " + e);
+        }
     }
     @Test
     public void checkGivenLastNameisInvalidForNumericVAlues() {
-        Assert.assertEquals(false, person.isLastNameValid("Amit123"));
+        try {
+            Assert.assertEquals(false, person.isLastNameValid("Amit123"));
+        }catch(Exception e) {
+            System.out.println("Exception occured is " + e);
+        }
     }
     @Test
     public void checkGivenEmailisValid() { // Checks for valid email
-        Assert.assertEquals(true,person.isEmailValid("abc.xyz@gmail.com"));
+        try {
+            Assert.assertEquals(true, person.isEmailValid("abc.xyz@gmail.com"));
+        }catch(Exception e) {
+            System.out.println("Exception occured is " + e);
+        }
     }
     @Test
     public void checkGivenEmailisInvalid() { // Checks for invalid email
-        Assert.assertEquals(false, person.isEmailValid("abc..xyz@gmail.com"));
+        try {
+            Assert.assertEquals(false, person.isEmailValid("abc..xyz@gmail.com"));
+        }catch(Exception e) {
+            System.out.println("Exception occured is " + e);
+        }
     }
     @Test
     public void checkGivenMobileNumberisValid() { // Checks For valid number
-        Assert.assertEquals(true, person.isMobileNumberValid("91 8210029078"));
+        try {
+            Assert.assertEquals(true, person.isMobileNumberValid("91 8210029078"));
+        }catch(Exception e) {
+            System.out.println("Exception occured is " + e);
+        }
     }
     @Test
     public void checkGivenMobileNumberisInvalidWithoutSpace() { // Checks the space between country code and the number
-        Assert.assertEquals(false, person.isMobileNumberValid("918210029078"));
+        try {
+            Assert.assertEquals(false, person.isMobileNumberValid("918210029078"));
+        }catch(Exception e) {
+            System.out.println("Exception occured is " + e);
+        }
     }
     @Test
     public void checkGivenMobileNumberisInvalidWithLessThan10Digits() { // Checks for less than 10 digits in number after country code
-        Assert.assertEquals(false, person.isMobileNumberValid("91 8210078"));
+        try {
+            Assert.assertEquals(false, person.isMobileNumberValid("91 8210078"));
+        }catch(Exception e) {
+            System.out.println("Exception occured is " + e);
+        }
     }
     @Test
     public void checkGivenMobileNumberisInvalidWithNot91InFirsttwoPlaces() { // checks for country code other than 91
-        Assert.assertEquals(false, person.isMobileNumberValid("52 8210029078"));
+        try {
+            Assert.assertEquals(false, person.isMobileNumberValid("52 8210029078"));
+        }catch(Exception e) {
+            System.out.println("Exception occured is " + e);
+        }
     }
     @Test
     public void checkGivenMobileNumberisInvalidWithFirstCharacterAfterSpaceIsLessThan() { // checks for first number after country code is more than 6
-        Assert.assertEquals(false, person.isMobileNumberValid("91 4210029078"));
+        try {
+            Assert.assertEquals(false, person.isMobileNumberValid("91 4210029078"));
+        }catch(Exception e) {
+            System.out.println("Exception occured is " + e);
+        }
     }
     @Test
     public void checkGivenPasswordIsValid() { // checks for valid password
-        Assert.assertEquals(true, person.isPasswordValid("Asad@12Sad"));
+        try {
+            Assert.assertEquals(true, person.isPasswordValid("Asad@12Sad"));
+        }catch(Exception e) {
+            System.out.println("Exception occured is " + e);
+        }
     }
     @Test
     public void checkGivenPasswordISInvalidforLessThan8Characters() { //checks for password less than 8 characters
-        Assert.assertEquals(false, person.isPasswordValid("Ad@12S"));
+        try {
+            Assert.assertEquals(false, person.isPasswordValid("Ad@12S"));
+        }catch(Exception e) {
+            System.out.println("Exception occured is " + e);
+        }
     }
     @Test
     public void checkGivenPasswordISInvalidforNoUpperCaseCharacter() { // checks for password  with no upper case
-        Assert.assertEquals(false, person.isPasswordValid("asd@12dffgd"));
+        try {
+            Assert.assertEquals(false, person.isPasswordValid("asd@12dffgd"));
+        }catch(Exception e) {
+            System.out.println("Exception occured is " + e);
+        }
     }
     @Test
     public void checkGivenPasswordISInvalidforNoLowercaseCaracters() { // checks for password  with no lower case
-        Assert.assertEquals(false, person.isPasswordValid("ASFG@2512S"));
+        try {
+            Assert.assertEquals(false, person.isPasswordValid("ASFG@2512S"));
+        }catch(Exception e) {
+            System.out.println("Exception occured is " + e);
+        }
     }
     @Test
     public void checkGivenPasswordISInvalidforNoSpecialCharacters() { //checks for password with no special characters
-        Assert.assertEquals(false, person.isPasswordValid("AdjgdsS512S"));
+        try {
+            Assert.assertEquals(false, person.isPasswordValid("AdjgdsS512S"));
+        }catch(Exception e) {
+            System.out.println("Exception occured is " + e);
+        }
     }
 }
